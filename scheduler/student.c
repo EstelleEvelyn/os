5710acb0f271b3db7e1e1ebe642aaeecf3d8bda4
EstelleEvelyn <estelle.bayer@gmail.com>
2017-10-31 06:01:01 -0500

    er'

--- scheduler/student.c
+++ scheduler/student.c
@@ -226,12 +226,12 @@ extern void wake_up(pcb_t *process) {
     process->state = PROCESS_READY;
     addReadyProcess(process);
     int preempt_cpu = getLowerPriority(process);
-    // pthread_mutex_lock(&current_mutex);
     if (preempt_cpu != -1) {
+      pthread_mutex_lock(&current_mutex);
       force_preempt(preempt_cpu);
       current[preempt_cpu] = process;
-    // pthread_mutex_unlock(&current_mutex);
-    process->state = PROCESS_RUNNING;
+      pthread_mutex_unlock(&current_mutex);
+      process->state = PROCESS_RUNNING;
     }
 }
 
