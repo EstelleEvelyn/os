e4b0d1c15c515cde7225309c0abb54e6cb758ff5
EstelleEvelyn <estelle.bayer@gmail.com>
2017-10-31 15:02:41 -0500

    eif'

--- scheduler/student.c
+++ scheduler/student.c
@@ -289,6 +289,8 @@ static void addReadyProcess(pcb_t* proc) {
 
     // ensure that this proc points to NULL
     proc->next = NULL;
+    pthread_mutex_unlock(&ready_mutex)
+    return NULL;
   } else {
     if(1 > proc->temp_priority || proc->temp_priority > 4) {
       proc->temp_priority = 1;
@@ -333,6 +335,7 @@ static void addReadyProcess(pcb_t* proc) {
     }
     proc->next = NULL;
     pthread_mutex_unlock(&ready_mutex);
+    return NULL;
   }
 }
 
