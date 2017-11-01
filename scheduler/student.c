2f767d79929188d80417c5889ff1859e6a22d741
EstelleEvelyn <estelle.bayer@gmail.com>
2017-10-31 19:13:43 -0500

    eif'

--- scheduler/student.c
+++ scheduler/student.c
@@ -303,7 +303,7 @@ static void addReadyProcess(pcb_t* proc) {
       if(head4 == NULL) {
         head4 = proc;
         tail4 = proc;
-        pthread_cond_broadcast(&ready_empty);
+        pthread_cond_signal(&ready_empty);
       } else {
         tail4->next = proc;
         tail4 = proc;
@@ -312,7 +312,7 @@ static void addReadyProcess(pcb_t* proc) {
       if(head3 == NULL) {
         head3 = proc;
         tail3 = proc;
-        pthread_cond_broadcast(&ready_empty);
+        pthread_cond_signal(&ready_empty);
       } else {
         tail3->next = proc;
         tail3 = proc;
@@ -321,7 +321,7 @@ static void addReadyProcess(pcb_t* proc) {
       if (head2 == NULL) {
         head2 = proc;
         tail2 = proc;
-        pthread_cond_broadcast(&ready_empty);
+        pthread_cond_signal(&ready_empty);
       } else {
         tail2->next = proc;
         tail2 = proc;
@@ -330,7 +330,7 @@ static void addReadyProcess(pcb_t* proc) {
       if(head == NULL) {
         head = proc;
         tail = proc;
-        pthread_cond_broadcast(&ready_empty);
+        pthread_cond_signal(&ready_empty);
       } else {
         tail->next = proc;
         tail = proc;
@@ -379,7 +379,7 @@ static int getLowerPriority(pcb_t *process) {
   int curr_cpu = 0;
   while(current[curr_cpu]) {
     pcb_t* compare_process = current[curr_cpu];
-    if(compare_process->static_priority < process->static_priority) {
+    if(compare_process != NULL && compare_process->static_priority < process->static_priority) {
       return curr_cpu;
     }
     curr_cpu++;
