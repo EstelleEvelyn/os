9292ea48f0781737dbb5d4cf114a8a11382f85cf
EstelleEvelyn <estelle.bayer@gmail.com>
2017-11-03 02:19:48 -0500

    ete

--- scheduler/student.c
+++ scheduler/student.c
@@ -131,7 +131,7 @@ int main(int argc, char *argv[])
 extern void idle(unsigned int cpu_id)
 {
   pthread_mutex_lock(&ready_mutex);
-  while (head == NULL && (alg != MultiLevelPrio || ( head2 == NULL && head3 == NULL & head4 == NULL))  {
+  while (head == NULL && (alg != MultiLevelPrio || ( head2 == NULL && head3 == NULL & head4 == NULL)))  {
     printf("waiting\n");
     pthread_cond_wait(&ready_empty, &ready_mutex);
   }
