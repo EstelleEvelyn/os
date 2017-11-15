40c598157b524d562d6162d9e256341d72479012
EstelleEvelyn <estelle.bayer@gmail.com>
2017-11-15 06:44:12 -0600

    er

--- memory/student-src/page-lookup.c
+++ memory/student-src/page-lookup.c
@@ -13,17 +13,17 @@
 pfn_t pagetable_lookup(vpn_t vpn, int write) {
    pfn_t pfn = 0;
 
-   /* FIX ME - Part 2
-    * Determine the PFN corresponding to the passed in VPN.
-    * current_pagetable variable accesses the process page table.
-    * if the pagetable entry is not valid:
-	*   increment count_pagefaults
-	*   call pagefault_handler() (partially implemented already,
-	*        found in file page-fault.c, returns frame #) to get pfn
-	*   change pagetable entry to now be valid
-    */
-// pte_t pte = current_pagetable[vpn];
-// pfn = pte->pfn;
+//    /* FIX ME - Part 2
+//     * Determine the PFN corresponding to the passed in VPN.
+//     * current_pagetable variable accesses the process page table.
+//     * if the pagetable entry is not valid:
+// 	*   increment count_pagefaults
+// 	*   call pagefault_handler() (partially implemented already,
+// 	*        found in file page-fault.c, returns frame #) to get pfn
+// 	*   change pagetable entry to now be valid
+//     */
+// // pte_t pte = current_pagetable[vpn];
+// // pfn = pte->pfn;
     pfn = current_pagetable->pfn;
     if (current_pagetable[vpn]->valid != 1) {
       count_pagefaults++;
